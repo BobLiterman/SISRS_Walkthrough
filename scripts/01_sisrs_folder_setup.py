de7778b83698bb7672d59dffd84e285b319682fd
BobLiterman <jazz710@gmail.com>
2020-05-27 12:10:37 -0400

    parse 01

--- scripts/01_sisrs_folder_setup.py
+++ scripts/01_sisrs_folder_setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
 # This script preps the folder architecture for a SISRS run.
-# Taxon ID file (text file with Taxon IDs on new lines) must be in base SISRS folder
-# Arguments: (1) Path to Taxon ID file (text file with Taxon IDs on new lines)
-# Example: python scripts/folder_setup.py TaxonIDs
+# Arguments: (1) tid: Path to text file with Taxon IDs on new lines [Default: One level above 'scripts' directory]
+# Example: python scripts/folder_setup.py --tid /path/to/TaxonIDs
 # Output: Script will create lots of folders, including taxon folders in the RawReads, TrimReads, and SISRS_Run folders
 
 import sys
 import os
 from os import path
+import argparse
 
-taxon_ID_file = path.abspath(sys.argv[1])
-sisrs_dir = path.dirname(taxon_ID_file)
+# Set script dir and SISRS dir location 
+script_dir = sys.path[0]
+base_dir = path.dirname(path.abspath(script_dir))
+
+# Get taxon ID file location
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('tid',action='store',default=path.dirname(path.abspath(script_dir))+"/Taxon_IDs")
+args = my_parser.parse_args()
+taxon_ID_file = args.tid
+
+# Read taxon IDs from file
 taxa_list = [line.rstrip('\n') for line in open(taxon_ID_file)]
 
+# Make directories
 os.mkdir(sisrs_dir+"/Reads")
 os.mkdir(sisrs_dir+"/Reads/RawReads")
 os.mkdir(sisrs_dir+"/Reads/TrimReads")
