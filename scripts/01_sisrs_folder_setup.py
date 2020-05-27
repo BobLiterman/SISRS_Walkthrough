c525530a7a57a4fc1e72216de3c0e99be3406d8c
BobLiterman <jazz710@gmail.com>
2020-05-27 12:27:09 -0400

    ArgParse

--- scripts/01_sisrs_folder_setup.py
+++ scripts/01_sisrs_folder_setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 # This script preps the folder architecture for a SISRS run.
-# Arguments: (1) tid: Path to text file with Taxon IDs on new lines [Default: One level above 'scripts' directory]
-# Example: python scripts/folder_setup.py --tid /path/to/TaxonIDs
+# Arguments: (1) --tid: Path to text file with Taxon IDs on new lines [Default: One level above 'scripts' directory]
+# To fetch taxon IDs from 'Taxon_IDs' file in SISRS base directory: python scripts/folder_setup.py 
+# To fetch taxon IDs from another directory: python scripts/folder_setup.py --tid /some/other/path/to/Taxon_IDs
 # Output: Script will create lots of folders, including taxon folders in the RawReads, TrimReads, and SISRS_Run folders
 
 import sys
@@ -16,7 +17,7 @@ base_dir = path.dirname(path.abspath(script_dir))
 
 # Get taxon ID file location
 my_parser = argparse.ArgumentParser()
-my_parser.add_argument('tid',action='store',default=path.dirname(path.abspath(script_dir))+"/Taxon_IDs")
+my_parser.add_argument('--tid',action='store',default=path.dirname(path.abspath(script_dir))+"/Taxon_IDs",nargs="?")
 args = my_parser.parse_args()
 taxon_ID_file = args.tid
 
@@ -24,14 +25,14 @@ taxon_ID_file = args.tid
 taxa_list = [line.rstrip('\n') for line in open(taxon_ID_file)]
 
 # Make directories
-os.mkdir(sisrs_dir+"/Reads")
-os.mkdir(sisrs_dir+"/Reads/RawReads")
-os.mkdir(sisrs_dir+"/Reads/TrimReads")
-os.mkdir(sisrs_dir+"/Reads/SubsetReads")
-os.mkdir(sisrs_dir+"/SISRS_Run")
-os.mkdir(sisrs_dir+"/Reference_Genome")
+os.mkdir(base_dir+"/Reads")
+os.mkdir(base_dir+"/Reads/RawReads")
+os.mkdir(base_dir+"/Reads/TrimReads")
+os.mkdir(base_dir+"/Reads/SubsetReads")
+os.mkdir(base_dir+"/SISRS_Run")
+os.mkdir(base_dir+"/Reference_Genome")
 
 for x in taxa_list:
-    os.mkdir(sisrs_dir+"/Reads/RawReads/"+x)
-    os.mkdir(sisrs_dir+"/Reads/TrimReads/"+x)
-    os.mkdir(sisrs_dir+"/SISRS_Run/"+x)
+    os.mkdir(base_dir+"/Reads/RawReads/"+x)
+    os.mkdir(base_dir+"/Reads/TrimReads/"+x)
+    os.mkdir(base_dir+"/SISRS_Run/"+x)
