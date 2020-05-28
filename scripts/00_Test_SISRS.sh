7edca53954f6d62ad07d2a3e8bf7953ba04c8f7e
BobLiterman <jazz710@gmail.com>
2020-05-28 17:02:57 -0400

    Add slicer

--- scripts/00_Test_SISRS.sh
+++ scripts/00_Test_SISRS.sh
@@ -9,16 +9,16 @@ python 01_sisrs_folder_setup.py --tid Test_Data/Taxon_IDs
 cp -rf Test_Data/RawReads/* ../Reads/RawReads/
 
 # Trim reads with FastQC analysis with 1 processor
-python 02_sisrs_read_trimmer.py --processors 7
+python 02_sisrs_read_trimmer.py --processors 1
 
 # Subset reads for composite genome assembly (assuming a 50Kb genome)
 python 03_sisrs_read_subsetter.py --genomesize 50000
 
 # Assemble composite genome with 1 node and 1 processor
-python 04_sisrs_composite_genome_assembly.py --nodes 1 --processors 7
+python 04_sisrs_composite_genome_assembly.py --nodes 1 --processors 1
 
 # Map and call bases for each species with 1 processor, requiring 2 reads of coverage (minread 2) and all reads supporting a single base (threshold 1)
-python 05_sisrs_run_prep.py --processors 7 --minread 2 --threshold 1
+python 05_sisrs_run_prep.py --processors 1 --minread 2 --threshold 1
 python 06_sisrs_run_mapping.py
 
 # Output final alignments, and filter datasets down to those with 0, 1, or 2 species missing per site
